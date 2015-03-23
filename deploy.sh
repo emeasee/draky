3d705affd17c92a3ee909742f46ad1f292be8166
Mac Oosthuizen <hello@m-jo.co>
2015-03-23 15:41:12 +0000

    More updating to catch up to gcloud changes

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+KUBE_CMD=gcloud preview containers kubectl
+
+# Update Kubernetes replicationController
+envsubst < kubernetes/web-controller.json.template > web-controller.json
+$KUBE_CMD -c web-controller.json \
+  update replicationControllers/webcontroller
+
+# Roll over Kubernetes pods
+$KUBE_CMD rollingupdate webcontroller
